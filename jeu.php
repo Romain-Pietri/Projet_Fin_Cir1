fde5be52006e9295208a37844f851b2df04d0453
Alixe MAERTE <alixe.maerte@student.junia.com>
2022-05-10 14:56:25 +0200

    envoi

--- jeu.php
+++ jeu.php
@@ -1,8 +1,52 @@
-<?php
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>A vos loupes !</title>
+</head>
+<body>
+    <header>
+    <?php
     session_start();
 
     require "header.php";
-?>
+    ?>
+
+    <button>
+    <a href="connexion.php">
+    Se connecter
+    </a>
+    </button>
+
+    
+    <button>
+    <a href="règles.php">
+    Voir les règles
+    </a>
+    </button>
+
+    </header>
+
+    <main>
 
 
-<?php require 'footer.php'; ?>
+
+
+
+
+
+
+
+    </main>
+
+    <footer>
+    <?php require 'footer.php'; ?>
+
+
+
+    </footer>
+
+
+</body>
+</html>
