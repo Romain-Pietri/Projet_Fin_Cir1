d4b064fd272e9744ffceb42269ba29eb946ccee8
Romain <romain.pietri@student.junia.com>
2022-05-12 12:21:28 +0200

    PUTAIN CA MARCHE

--- jeu.php
+++ jeu.php
@@ -1,19 +1,32 @@
-    <?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>ATESPUTAINDELOUPE</title>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+
+</head>
+<?php
     session_start();
 
     require "header.php";
     ?>
+<body>
 
-    <main>
+    <div id="x"></div>
 
+    <script>
 
-        <js> </js>
+    var img = document.createElement("img");
+    img.src = "./Images-jeu/maison.png";
 
+    var div = document.getElementById("x");
+    div.appendChild(img);
+    div.setAttribute("style", "text-align:center");
 
+    </script>
 
-
-
-
-    </main>
-
+</body>
 <?php require 'footer.php'; ?>
+
+</html>
