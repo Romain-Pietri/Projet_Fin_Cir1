cde38a5340e740e906de6238c17ce2fe62f161ac
AugusteBuriez <90209422+AugusteBuriez@users.noreply.github.com>
2022-05-13 17:04:09 +0200

    selection taille jeu

--- jeu.php
+++ jeu.php
@@ -5,17 +5,35 @@
     ?>
 </header>
 <body>
+<?php
+    if(!isset($_POST["Envoyer"])){
+        echo '<legend class="input">Choisissez la taille de la grille :</legend>
+    <form method="post" action="jeu.php">
+        <input type="radio" name="taille" id="taille" value="4"/>
+			<label for="4x4" class="input">4x4</label>
+		<input type="radio" name="taille" id="taille" value="6"/>
+			<label for="6x6" class="input">6x6</label>
+        <input type="radio" name="taille" id="taille" value="8"/>
+			<label for="8x8" class="input">8x8</label>    
+        <input type="submit" name="Envoyer" Value="Envoyer"/>
+    </form>';
+        
+    }
+    else {
+        $taille = $_POST["taille"];
+        echo $taille;
+    }
+?>
 
-    <div id="x"></div>
 
     <script>
 
-    var img = document.createElement("img");
+    /*var img = document.createElement("img");
     img.src = "./Images-jeu/maison.png";
 
     var div = document.getElementById("x");
     div.appendChild(img);
-    div.setAttribute("style", "text-align:center");
+    div.setAttribute("style", "text-align:center");*/
 
     </script>
 
