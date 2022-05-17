db7a5a57d41bbaae0aada28e05a15bc305c1d72e
RaphSoko <raphael.sokolowski@student.junia.com>
2022-05-17 16:13:42 +0200

    rickroll

--- footer.php
+++ footer.php
@@ -5,9 +5,9 @@
 	      <dd>(+33) (0) 6 24 31 06 38</dd>
 	  </dl>
 	    <ul class="icon">
-	      <a href="http://facebook.com/Avoloupes/"><img class="reseaux" src="images/facebook.png" alt="fb"/></a>
-	      <a href="http://twitter.com/Avoloupes/"><img class="reseaux" src="images/twitter.png" alt="fb"/></a>
-	      <a href="http://instagram.com/Avoloupes/"><img class="reseaux" src="images/instagram.png" alt="fb"/></a>
+	      <a href="http://facebook.com/Takuzu/"><img class="reseaux" src="images/facebook.png" alt="fb"/></a>
+	      <a href="http://twitter.com/Takuzu/"><img class="reseaux" src="images/twitter.png" alt="fb"/></a>
+	      <a href="http://instagram.com/Takuzu/"><img class="reseaux" src="images/instagram.png" alt="fb"/></a>
 	    </ul>
 	  <br>
 	      <p>Website designed by Romain, Auguste, Raphael Alexandre and Alixe</p>
