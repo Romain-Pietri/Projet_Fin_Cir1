314305b2c1b807934fb64522fccaafe37d7bf2ab
Romain <romain.pietri@student.junia.com>
2022-05-18 10:57:32 +0200

    fonction intéligente finit, mise en phase de teste

--- Programmes-C/Solver.c
+++ Programmes-C/Solver.c
@@ -4,7 +4,6 @@
 #include "Solver.h"
 
 
-
 /*
 Alloue de la mémoire dynamiquement pour la grille
 */
@@ -104,7 +103,6 @@ bool isUniqueCol(Grille *g,int col){
     }
     return true;
 }
-
 /*
 Verifie si la ligne est unique
 */
@@ -124,7 +122,6 @@ bool isUniqueLigne(Grille *g,int ligne){
     }
     return true;
 }
-
 /*
 Vérifie si on peut placer la valeurs a cet endroit
 */
@@ -338,12 +335,150 @@ Grille *Solve(Grille *g, int ligne, int col){
 Remplie la grille avec les solutions facile à trouver
 */
 bool inteligent(Grille *g){
+    for(int i=0; i<g->taille; ++i){
+        if(countElemCol(g, i, 0)==g->taille/2 && countElemCol(g, i, 1)!=g->taille/2){
+            for(int j=0; j<g->taille; ++j){
+                if(g->tab[i][j]==-1){
+                    g->tab[i][j]=0;
+                }
+            }
+            return true;
+        }
+        if(countElemLigne(g, i, 0)==g->taille/2 && countElemLigne(g, i, 1)!=g->taille/2){
+            for(int j=0; j<g->taille; ++j){
+                if(g->tab[j][i]==-1){
+                    g->tab[j][i]=0;
+                }
+            }
+            return true;
+        }
+        if(countElemCol(g, i, 0)==g->taille/2 && countElemCol(g, i, 1)==g->taille/2){
+            for(int j=0; j<g->taille; ++j){
+                if(g->tab[i][j]==-1){
+                    g->tab[i][j]=1;
+                }
+            }
+            return true;
+        }
+        if(countElemLigne(g, i, 0)==g->taille/2 && countElemLigne(g, i, 1)==g->taille/2){
+            for(int j=0; j<g->taille; ++j){
+                if(g->tab[j][i]==-1){
+                    g->tab[j][i]=1;
+                }
+            }
+            return true;
+        }
+        for(int j=0; j<g->taille; ++j){
+            if(g->tab[i][j]==-1){           
+                if(i!=0 && i!=g->taille-1){
+                    if(g->tab[i-1][j]==0 && g->tab[i+1][j]==0){
+                        g->tab[i][j]=0;
+                        return true;
+                    }
+                    else if(g->tab[i-1][j]==1 && g->tab[i+1][j]==1){
+                        g->tab[i][j]=1;
+                        return true;
+                    }
+                }
+                if(j!=0 && j!=g->taille-1){
+                    if(g->tab[i][j-1]==0 && g->tab[i][j+1]==0){
+                        g->tab[i][j]=0;
+                        return true;
+                    }
+                    else if(g->tab[i][j-1]==1 && g->tab[i][j+1]==1){
+                        g->tab[i][j]=1;
+                        return true;
+                    }
+                }
+                if(i<=g->taille-3){
+                    if(g->tab[i+1][j]==0 && g->tab[i+2][j]==0){
+                        g->tab[i][j]=0;
+                        if(i+3<g->taille){
+                            if(g->tab[i+3][j]==-1){
+                                g->tab[i+3][j]=0;
+                            }
+                        }
+                        return true;
+                    }
+                    else if(g->tab[i+1][j]==1 && g->tab[i+2][j]==1){
+                        g->tab[i][j]=1;
+                        if(i+3<g->taille){
+                            if(g->tab[i+3][j]==-1){
+                                g->tab[i+3][j]=1;
+                            }
+                        }
+                        return true;
+                    }
+                }
+                if(j<=g->taille-3){
+                    if(g->tab[i][j+1]==0 && g->tab[i][j+2]==0){
+                        g->tab[i][j]=0;
+                        if(j+3<g->taille){
+                            if(g->tab[i][j+3]==-1){
+                                g->tab[i][j+3]=0;
+                            }
+                        }
+                        return true;
+                    }
+                    else if(g->tab[i][j+1]==1 && g->tab[i][j+2]==1){
+                        g->tab[i][j]=1;
+                        if(j+3<g->taille){
+                            if(g->tab[i][j+3]==-1){
+                                g->tab[i][j+3]=1;
+                            }
+                        }
+                        return true;
+                    }
+                }
+                if(i>=2){
+                    if(g->tab[i-1][j]==0 && g->tab[i-2][j]==0){
+                        g->tab[i][j]=0;
+                        if(i-3>=0){
+                            if(g->tab[i-3][j]==-1){
+                                g->tab[i-3][j]=0;
+                            }
+                        }
+                        return true;
+                    }
+                    else if(g->tab[i-1][j]==1 && g->tab[i-2][j]==1){
+                        g->tab[i][j]=1;
+                        if(i-3>=0){
+                            if(g->tab[i-3][j]==-1){
+                                g->tab[i-3][j]=1;
+                            }
+                        }
+                        return true;
+                    }
+                }
+                if(j>=2){
+                    if(g->tab[i][j-1]==0 && g->tab[i][j-2]==0){
+                        g->tab[i][j]=0;
+                        if(j-3>=0){
+                            if(g->tab[i][j-3]==-1){
+                                g->tab[i][j-3]=0;
+                            }
+                        }
+                        return true;
+                    }
+                    else if(g->tab[i][j-1]==1 && g->tab[i][j-2]==1){
+                        g->tab[i][j]=1;
+                        if(j-3>=0){
+                            if(g->tab[i][j-3]==-1){
+                                g->tab[i][j-3]=1;
+                            }
+                        }
+                        return true;
+                    }
+                }
+        }
+        }
+    }
+
     return false;
 }
 /*
 Resout la grille avec Backtracking et l'intelligence
 */
-
 bool Solveur(Grille *g){
     while(inteligent(g)){
         NULL;
