e0e1f656e5edc2382544a49bf5ddd4f79f67c5b0
RaphSoko <raphael.sokolowski@student.junia.com>
2022-05-20 15:28:01 +0200

    changement base

--- grillebase.php
+++ grillebase.php
@@ -1,10 +1,11 @@
 <?php
 require("connexiondb.php");
 
-$json = file_get_contents('data.json'); //récupérer le contenu du fichier
+        $json = file_get_contents('json.json'); //récupérer le contenu du fichier
         $data = json_decode($json, false); //"traduire" du json à des variables php
         $tab = $data->tableau; //enregistrer les variables de $data dans d'autres variables php 
-        $edata=json_encode($data, false); //encoder les variables sous forme json
-        file_put_contents("data.json",$edata); //changer le contenu du fichier
+        /*$edata=json_encode($data, false); //encoder les variables sous forme json
+        file_put_contents("json.json",$edata); //changer le contenu du fichier*/
+        $request="INSERT"
 
 ?>
