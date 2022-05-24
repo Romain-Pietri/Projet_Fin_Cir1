f844a23f381286f7618fd815ab87bdf08b2e9c8f
Alixe <alixe.maerte@student.junia.com>
2022-05-24 16:39:48 +0200

    page start jouli

--- start.php
+++ start.php
@@ -4,17 +4,22 @@
     require "header.php";
 ?>
 
-<h1>Starship<br>Invader</h1>
+<h1>Starship Invader</h1>
+<br>
 
-<p class="debut"><br><br>Hello young astronaut! I’m Captain Gadget <br> Welcome to Starship invader, our mission is to maintain the peace between us and the aliens helping them park their ships. <br>You will have more details in the parking rules. Let's start !<br><br><p>
+<img id="bulle" src="images/bulle.png">
+<img id="gadget" src="images/gadget1.gif">
 
 <?php
     if(isset($_SESSION["login"])){
-        echo'<a href="jeu.php"><img id="start" src="images/start.png"></a>';
+        echo "<a href='jeu.php'><img id=start' src='images/start.png'></a>";
     }
     else{
-        echo'<a href="connexion.php"><img id="start" src="images/start.png"></a>';
+        echo "<a href='connexion.php'><img id='start' src='images/start.png'></a>";
     }
 ?>
+
+
+<br>
 
 <?php require 'footer.php'; ?>
