2cc1e150d7445497e13e235618dd3c57bbdf0b49
Auguste BURIEZ <auguste.buriez@student.junia.com>
2022-05-25 14:23:22 +0200

    taille.php

--- jeu.php
+++ jeu.php
@@ -51,22 +51,11 @@
 <body>
 <?php
      
-    if(!isset($_SESSION['taille'])){
-        echo '<legend class="input">Choisissez la taille de la grille :</legend>
-    <form method="post" action="jeu.php">
-        <input type="radio" name="taille" id="taille" value="4"/>
-			<label for="4x4" class="input">4x4</label>
-		<input type="radio" name="taille" id="taille" value="6"/>
-			<label for="6x6" class="input">6x6</label>
-        <input type="radio" name="taille" id="taille" value="8"/>
-			<label for="8x8" class="input">8x8</label>    
-        <input type="submit" name="Envoyer" Value="Envoyer"/>
-    </form>';
-        
+    if(!isset($_POST["Envoyer"])){
+        header("taille.php");
     }
     else {
-        $taille = $_POST["taille"];
-        $_SESSION['taille'] = $taille;
+        $taille = $_SESSION["taille"];
     
     require("connexiondb.php");
 	    
---
+++ taille.php
@@ -0,0 +1,23 @@
+<?php  
+    session_start();
+
+    if(!isset($_SESSION["taille"]) && !isset($_POST["Envoyer"])){
+
+        echo '<legend class="input">Choisissez la taille de la grille :</legend>
+    <form method="post" action="jeu.php">
+        <input type="radio" name="taille" id="taille" value="4"/>
+			<label for="4x4" class="input">4x4</label>
+		<input type="radio" name="taille" id="taille" value="6"/>
+			<label for="6x6" class="input">6x6</label>
+        <input type="radio" name="taille" id="taille" value="8"/>
+			<label for="8x8" class="input">8x8</label>    
+        <input type="submit" name="Envoyer" Value="Envoyer"/>
+    </form>';
+        
+    }
+    else{
+        $_SESSION["taille"] = $_POST["taille"];
+        header("jeu.php");
+    }
+
+?>
