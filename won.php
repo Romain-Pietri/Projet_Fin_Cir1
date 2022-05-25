e5fca29f1b53cfd3cec2eff658cb0555c6db92bf
RaphSoko <raphael.sokolowski@student.junia.com>
2022-05-25 16:26:48 +0200

    page de victoire (sans css)

---
+++ won.php
@@ -0,0 +1,59 @@
+<?php
+    session_start();
+
+    require "header.php";
+    $taille=$_SESSION["taille"];
+    ?>
+
+<h1> Congratulations !</h1>
+</header>
+<main>
+<?php
+if ($taille==4){
+?>
+<div class="regles">
+<br>
+<br>
+<p>Congratulations ! You solved the grid, and now everyone is happy, youhou !</p>
+<br>
+<img id="dab" src="images/dab.gif">
+<br>
+<p> You can try to do the 6x6 and 8x8 grids, to get a better score and appear in the leaderboard !</p>
+<br>
+</div>
+
+<?php
+}
+else if($taille==6){
+?>
+<div class="regles">
+<br>
+<br>
+<p>Congratulations ! You solved the grid, and now everyone is happy, youhou !</p>
+<br>
+<img id="dab" src="images/dab.gif">
+<br>
+<p> You can try to do the 8x8 grid, to get a better score and appear in the leaderboard !</p>
+<br>
+</div>
+
+<?php
+}
+else if($taille==8){
+?>
+
+<div class="regles">
+<br>
+<br>
+<p>Congratulations ! You solved the biggest grid, and now everyone is happy, youhou !</p>
+<br>
+<img id="dab" src="images/dab.gif">
+<br>
+<p> You can try to do it faster, to get a better score and appear in the leaderboard !</p>
+<br>
+</div>
+</main>
+
+<?php
+    require "footer.php";
+?>
