7325752a21a2d63efa075f636d05491ece86951e
AugusteBuriez <90209422+AugusteBuriez@users.noreply.github.com>
2022-06-03 17:05:28 +0200

    update :cactus:

--- lost.php
+++ lost.php
@@ -22,5 +22,14 @@
 <br><br>
 
 <?php
+    $_COOKIE["Ligne1"] = "[0,0,0,0,0,0,0,0]";
+    $_COOKIE["Ligne2"] = "[0,0,0,0,0,0,0,0]";
+    $_COOKIE["Ligne3"] = "[0,0,0,0,0,0,0,0]";
+    $_COOKIE["Ligne4"] = "[0,0,0,0,0,0,0,0]";
+    $_COOKIE["Ligne5"] = "[0,0,0,0,0,0,0,0]";
+    $_COOKIE["Ligne6"] = "[0,0,0,0,0,0,0,0]";
+    $_COOKIE["Ligne7"] = "[0,0,0,0,0,0,0,0]";
+    $_COOKIE["Ligne8"] = "[0,0,0,0,0,0,0,0]";
+    require("save.php");
 	require "footer.php";
 ?>
