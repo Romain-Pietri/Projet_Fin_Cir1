189d7a28d40d47488c79f47c923e76ca4d554c9d
AugusteBuriez <90209422+AugusteBuriez@users.noreply.github.com>
2022-06-08 15:30:44 +0200

    un pb de - :cactus:

--- cookies.php
+++ cookies.php
@@ -14,9 +14,14 @@
 
 
 	if(isset($_POST["Generate"])){
-		$_SESSION["taille"] = $_POST["taille"] * 1;
-		setcookie("id" , 0 , time()+(365*24*3600));
-		header("Location: interface.php");
+		if(!isset($_POST["taille"])){
+			header("Location:taille.php");
+		}
+		else {
+			$_SESSION["taille"] = $_POST["taille"] * 1;
+			setcookie("id" , 0 , time()+(365*24*3600));
+			header("Location: interface.php");
+		}
 	}
 
 
