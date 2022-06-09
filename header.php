21b06f3267baea529e6685788644a2e79b0e0cb4
Alixe <alixe.maerte@student.junia.com>
2022-06-09 09:33:26 +0200

    bim

--- header.php
+++ header.php
@@ -45,7 +45,7 @@
 		<li><a href="shop.php">Shop</a></li>
 		</ul>	
 	</span>
-	<audio autoplay id="myAudio" src="images/music1.mp3" preload="auto">
+	<audio id="myAudio" src="images/music1.mp3" preload="auto">
 	</audio>
 	
 	<span id="barright" class="bar_right">	
