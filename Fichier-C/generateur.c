f3af1f61642ec57037c089bef1759c3e8148cac7
AugusteBuriez <90209422+AugusteBuriez@users.noreply.github.com>
2022-06-09 10:07:21 +0200

    test

--- Fichier-C/generateur.c
+++ Fichier-C/generateur.c
@@ -5,64 +5,75 @@
 #include <time.h>
 #include "Solver.h"
 #include <time.h>
-
-void randomFill(Grille *g,int taille){
+Grille* randomFill(Grille* g, int taille) {
     srand(time(NULL));
-    g->taille=taille;
-    int posx,posy,val;
-    int tour;
-    int upper;
-    int lower;
-    switch (taille) {
-        case 4:
-            upper=6;
-            lower=5;
-            break;
-        case 6:
-            upper=11;
-            lower=8;
-            break;
-        case 8:
-            upper=19;
-            lower=16;
-            break;
-
+    g->taille = taille;
+    int val;
+    int pos;
+    for (int i = 0; i < taille; ++i) {
+        val = rand() % 2;
+        g->tab[i][i] = val;
     }
-
-    tour=(rand()%(upper - lower + 1)) + lower;
-    for(int i=0;i<tour;++i){
-        do{
-            posx=rand()%g->taille;
-            posy=rand()%g->taille;
-            val=rand()%2;
-        }while(g->tab[posx][posy]!=-1 || !checkElem(g,posx,posy,val));
-        g->tab[posx][posy]=val;
-    }
-
+    //printGrille(g);
+    return Resoudre(g);
 }
-/*
-Reset la grilles avec des -1
-*/
-void resetGrid(Grille* grid){
-    for (int i=0;i<grid->taille;i++){
-        for (int j=0;j<grid->taille;j++){
-            grid->tab[i][j]=-1;
+
+void randomEmpty(Grille* g) {
+    srand(time(NULL));
+    int tour;
+    int posx, posy;
+    int upperx, lowerx;
+    int uppery, lowery;
+    switch (g->taille) {
+    case 4: tour = 2; break;
+    case 6: tour = 5; break;
+    case 8: tour = 10; break;
+    }
+    for (int i = 0; i < tour; ++i) {
+        for (int j = 0; j <= 3; ++j) {
+            switch (j) {
+            case 0: lowerx = 0; lowery = 0; upperx = g->taille / 2 - 1; uppery = g->taille / 2 - 1; break;
+
+            case 1: lowerx = g->taille / 2; lowery = 0; upperx = g->taille - 1; uppery = g->taille / 2 - 1; break;
+
+            case 2: lowerx = 0; lowery = g->taille / 2; upperx = g->taille / 2 - 1; uppery = g->taille - 1; break;
+
+            case 3: lowerx = g->taille / 2; lowery = g->taille / 2; upperx = g->taille - 1; uppery = g->taille - 1; break;
+            }
+            //printf("\n%d : \n",j);
+            //printf("%d %d %d %d\n",lowerx,upperx,lowery,uppery);
+            do {
+                posx = (rand() % (upperx - lowerx + 1)) + lowerx;
+                posy = (rand() % (uppery - lowery + 1)) + lowery;
+                //printf("%d | %d\n",posx,posy);
+            } while (g->tab[posy][posx] == -1);
+            g->tab[posy][posx] = -1;
+
         }
     }
+    //printGrille(g);
 }
-Grille * GenerateGrid(int taille){
-    Grille* tmp=Newgrille();
-    do{
-        resetGrid(tmp);
-        randomFill(tmp,taille);
-        
-    }while(!Solveur(cloneGrille(tmp)));
-    return tmp;
+void UniqueSolve(Grille* g) {
+    List* liste = Newlist();
+    while (nbsolution(g, liste) > 1) {
+        for (int i = 0; i < g->taille; ++i) {
+            for (int j = 0; j < g->taille; ++j) {
+                if (liste->head->g->tab[i][j] != liste->head->next->g->tab[i][j]) {
+                    g->tab[i][j] = liste->head->g->tab[i][j];
+                }
+            }
+        }
+        resetList(liste);
+    }
 }
 
-/*
-int main() {
-    
-    Grille * g=GenerateGrid(6);
-    printGrille(g);
-}*/
+
+
+
+Grille* GenerateGrid(int taille) {
+    Grille* tmp = Newgrille();
+    tmp = randomFill(tmp, taille);
+    randomEmpty(tmp);
+    UniqueSolve(tmp);
+    return tmp;
+}
