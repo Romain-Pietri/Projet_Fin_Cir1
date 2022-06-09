f0a3432bd3a31dde081c8e9133a364c03fa385fb
AugusteBuriez <90209422+AugusteBuriez@users.noreply.github.com>
2022-06-09 10:27:30 +0200

    du kou comme ça

--- cookies.php
+++ cookies.php
@@ -18,14 +18,14 @@
 	}
 
 	if(isset($_POST["Abandon"])){
-		$_COOKIE["Ligne1"] = [0,0,0,0,0,0,0,0];
-		$_COOKIE["Ligne2"] = [0,0,0,0,0,0,0,0];
-		$_COOKIE["Ligne3"] = [0,0,0,0,0,0,0,0];
-		$_COOKIE["Ligne4"] = [0,0,0,0,0,0,0,0];
-		$_COOKIE["Ligne5"] = [0,0,0,0,0,0,0,0];
-		$_COOKIE["Ligne6"] = [0,0,0,0,0,0,0,0];
-		$_COOKIE["Ligne7"] = [0,0,0,0,0,0,0,0];
-		$_COOKIE["Ligne8"] = [0,0,0,0,0,0,0,0];
+		$_COOKIE["Ligne1"] = "[0,0,0,0,0,0,0,0]";
+		$_COOKIE["Ligne2"] = "[0,0,0,0,0,0,0,0]";
+		$_COOKIE["Ligne3"] = "[0,0,0,0,0,0,0,0]";
+		$_COOKIE["Ligne4"] = "[0,0,0,0,0,0,0,0]";
+		$_COOKIE["Ligne5"] = "[0,0,0,0,0,0,0,0]";
+		$_COOKIE["Ligne6"] = "[0,0,0,0,0,0,0,0]";
+		$_COOKIE["Ligne7"] = "[0,0,0,0,0,0,0,0]";
+		$_COOKIE["Ligne8"] = "[0,0,0,0,0,0,0,0]";
 		require("save.php");
 		require("save2.php");
 		unset($_SESSION["taille"]);
